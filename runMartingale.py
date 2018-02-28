958d55f392f7cee58460f911d66181550ba8721d
lxt <910677221@qq.com>
2018-02-28 11:47:46 +0800

    log.txt

--- runMartingale.py
+++ runMartingale.py
@@ -18,7 +18,7 @@ def printlog(message):
 
 
 class BitmexWS:
-    def handleSupportAndPressurePrice(self,lastprice):
+    def handleSupportAndPressurePrice(self, lastprice):
         if lastprice < self.lowcontrolPriceline:
             if self.bc.pos > 0 and self.cengshu >= 4:
                 # 关键点位上，持仓逆向，且层数过高，平仓避避风头
@@ -34,7 +34,8 @@ class BitmexWS:
                         self.cengshu))
                 self.orderClose()
                 return True
-    def handlehaveLong(self,gap):
+
+    def handlehaveLong(self, gap):
         # 大于止盈点数，平仓
         if gap > self.targetProfit:
             printlog("持有多仓，超过盈利点数，平仓:" + str(gap))
@@ -48,7 +49,8 @@ class BitmexWS:
             else:
                 pass
                 # print("持有多仓，不触发平仓和加仓 gap = "+str(gap))
-    def handlehaveShort(self,gap):
+
+    def handlehaveShort(self, gap):
         if gap < -self.targetProfit:
             printlog("持有空仓，超过盈利点数，平仓:" + str(gap))
             self.orderClose()
@@ -61,11 +63,12 @@ class BitmexWS:
             else:
                 pass
                 # print("持有空仓，不触发平仓和加仓 gap = " + str(gap))
+
     def onMessage(self, message):
         a1 = message["data"]
         b = 'lastPrice' in a1[0]
         c = 'timestamp' in a1[0]
-        #过滤 websocket 信息，只需要最新价格
+        # 过滤 websocket 信息，只需要最新价格
         if b and c:
             lastprice = float(a1[0]['lastPrice'])
             timestamp = a1[0]['timestamp']
@@ -73,7 +76,7 @@ class BitmexWS:
             # sendToAll({
             #     "lastprice": lastprice
             # })
-            #如果存在仓位，gap就是当前仓位的盈利或者亏损的点数
+            # 如果存在仓位，gap就是当前仓位的盈利或者亏损的点数
             gap = lastprice - self.bc.avgPrice
             # 每十次websocket返回信息，就打印一次当前的状态信息
             if self.n % 10 == 0:
@@ -87,24 +90,24 @@ class BitmexWS:
                 return
 
             # 处理价格到了设定好的压力位，支撑位，平仓
-            if self.handleSupportAndPressurePrice(lastprice)==True:
+            if self.handleSupportAndPressurePrice(lastprice) == True:
                 return
 
             if self.prepos == 0:
                 printlog("无仓位立刻开仓")
                 self.order()
             else:
-                #为了处理没有仓位，gap会等于当前价格的时候
+                # 为了处理没有仓位，gap会等于当前价格的时候
                 if gap > 1000:
                     return
                 # 当前持有多仓
                 if self.prepos > 0:
-                  self.handlehaveLong(gap)
+                    self.handlehaveLong(gap)
                 # 当前持有空仓
                 elif self.prepos < 0:
                     self.handlehaveShort(gap)
 
-    #平仓
+    # 平仓
     def orderClose(self):
         self.isInOrder = True
         self.bc.orderClose()
@@ -112,7 +115,8 @@ class BitmexWS:
         self.mypos = 0
         self.init_jiacanggap = 10
         self.isPosChange = True
-    #下单
+    # 下单
+
     def order(self):
         self.isInOrder = True
         printlog("self.cengshu = " + str(self.cengshu))
@@ -147,14 +151,13 @@ class BitmexWS:
     def __init__(self):
         self.isRun = False
 
-
     def stopRun(sef):
         print('...')
 
     def startRun(self, settingidc):
         if(self.isRun):
             return
-        self.isRun = True        
+        self.isRun = True
         print('开始运行', settingidc)
 
         # 下限价格
@@ -199,6 +202,8 @@ class BitmexWS:
         self.prepos = pos
         orderBook10 = self.XBTH17.get_table('instrument')
         self.XBTH17.on('action', self.onMessage)
+
+
 # 静态文件服务器
 import http.server
 import threading
@@ -220,24 +225,24 @@ parse = json.JSONDecoder().decode
 clients = []
 bws = BitmexWS()
 
+
 def sendToAll(obj):
     str = stringify(obj)
     for ws in clients:
         asyncio.get_event_loop().create_task(ws.send(str))
 
+
 async def hello(ws, path):
-    print('+')
-    clients.append(ws)    
+    print('join')
+    clients.append(ws)
     while True:
         try:
             str = await ws.recv()
-            print('recv',str)
-            dic = parse(str)
-            print('recv',dic)
-            bws.startRun(dic)
-        except:
-            print('-')
-            clients.remove(ws)            
+            print('recv')
+            bws.startRun(parse(str))
+        except(e):
+            print('exit', e)
+            clients.remove(ws)
             break
 
 asyncio.get_event_loop().run_until_complete(
